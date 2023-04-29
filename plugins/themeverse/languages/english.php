1fdcb3f85933b6375894d1068c911f185ad3c64d
danuthintariu <hintariu.d@gmail.com>
2023-04-29 15:06:50 +0100

    Update english.php

--- plugins/themeverse/languages/english.php
+++ plugins/themeverse/languages/english.php
@@ -7,3 +7,5 @@ define('TEXT_PLUGIN_CURRENT','Your current version is');
 define('TEXT_PLUGIN_CHECK_VFS','Could not find the version in file');
 define('TEXT_PLUGIN_FILE_CK','File');
 define('TEXT_PLUGIN_FILE_CKC','could not be found');
+define('TEXT_PLUGIN_NO_FILE','No Themeverse skin could be found installed in directory');
+define('TEXT_PLUGIN_GIT_NO','This skin was not found on github');
