6a358e71a3ab4995a7570e86cf69270b5bd4b6b5
danuthintariu <hintariu.d@gmail.com>
2023-05-06 14:18:08 +0100

    Update themeverse_class.php

--- plugins/themeverse/classes/themeverse_class.php
+++ plugins/themeverse/classes/themeverse_class.php
@@ -7,13 +7,14 @@ class user_class
     global $app_user;
     
     $html = '';
-    $html .=  '<div>'.TEXT_USER_ID.': <b>' . $app_user['id'] . '</b></div>';
-    $html .= '<div>'.TEXT_USER_ACCG_ID.': <b>' . $app_user['group_id'] . '</b></div>';
+    $html .=  '<div>'.TEXT_USER_ID.': <b>'.$app_user['id'].'</b></div>';
+    $html .= '<div>'.TEXT_USER_ACCG_ID.': <b>'.$app_user['group_id'].'</b></div>';
     
-    $user_info_query = db_query("select * from app_entity_1 where id='" . $app_user['id'] . "'");
+    $user_info_query = db_query("select * from app_entity_1 where id='".$app_user['id']."'");
     if($user_info = db_fetch_array($user_info_query))
     {
-      $html .= '<div>'.TEXT_USER_NAME.': <b>' . $user_info['field_12'] . '</b></div>'; 
+      $html .= '<div>'.TEXT_USER_NAME.': <b>'.$user_info['field_12'].'</b></div>';
+      $html .= '<div>'.TEXT_USER_SKIN.': '.$user_info['field_14'].'</div>';
     }
     
     return $html;
