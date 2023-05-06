aa4e7a2451aec048a3b3a2bf90b08c3a34c8b2a1
danuthintariu <hintariu.d@gmail.com>
2023-05-06 09:32:07 +0100

    Update themeverse_class.php

--- plugins/themeverse/classes/themeverse_class.php
+++ plugins/themeverse/classes/themeverse_class.php
@@ -7,13 +7,13 @@ class user_class
     global $app_user;
     
     $html = '';
-    $html .=  '<div>User ID: <b>' . $app_user['id'] . '</b></div>';
-    $html .= '<div>User Access Group ID: <b>' . $app_user['group_id'] . '</b></div>';
+    $html .=  '<div>'.TEXT_USER_ID.': <b>' . $app_user['id'] . '</b></div>';
+    $html .= '<div>'.TEXT_USER_ACCG_ID.': <b>' . $app_user['group_id'] . '</b></div>';
     
     $user_info_query = db_query("select * from app_entity_1 where id='" . $app_user['id'] . "'");
     if($user_info = db_fetch_array($user_info_query))
     {
-      $html .= '<div>Username: <b>' . $user_info['field_12'] . '</b></div>'; 
+      $html .= '<div>'.TEXT_USER_NAME.': <b>' . $user_info['field_12'] . '</b></div>'; 
     }
     
     return $html;
