ffa48369045ef794f0ab3ef8168188152ff91983
danuthintariu <hintariu.d@gmail.com>
2023-05-06 09:32:14 +0100

    Update english.php

--- plugins/themeverse/languages/english.php
+++ plugins/themeverse/languages/english.php
@@ -1,5 +1,5 @@
 <?php
-
+// App
 define('TEXT_PLUGIN_TITLE','Themeverse');
 define('TEXT_PLUGIN_LATEST','You use the latest version');
 define('TEXT_PLUGIN_NEW','New version avaiable');
@@ -8,6 +8,10 @@ define('TEXT_PLUGIN_CHECK_VFS','Could not find the version in file');
 define('TEXT_PLUGIN_FILE_CK','File');
 define('TEXT_PLUGIN_FILE_CKC','could not be found');
 define('TEXT_PLUGIN_NO_FILE','No Themeverse skin could be found installed in directory');
-define('TEXT_PLUGIN_GIT_NO','This skin could not bo found on GitHub');
+define('TEXT_PLUGIN_GIT_NO','This skin could not be found on GitHub');
 define('TEXT_PLUGIN_GIT_CUR','Current version on GitHub');
 define('TEXT_PLUGIN_GIT_NEW','New version of Themeverse is available on GitHub');
+// User info
+define('TEXT_USER_ID','Your User ID');
+define('TEXT_USER_ACCG_ID','User Access Group ID');
+define('TEXT_USER_NAME','Your Username');
